b8113285b8431c01e1b34b0f7f8bf00b03e64db4
apastan <apastan.dev@gmail.com>
2024-09-19 23:51:48 +0300

    chore: install font Inter
    - install @fontsource/inter
    - import font to project
    - import font to storybook
    - set font-family in global.css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,10 @@
 import type { Preview } from '@storybook/react'
 
 import '@/styles/globals.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 
 const preview: Preview = {
   parameters: {
