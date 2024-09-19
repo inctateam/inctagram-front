eb34f6d6afbb95a971af7bf53723c470093acdcc
apastan <apastan.dev@gmail.com>
2024-09-19 19:10:06 +0300

    chore: install eslint, stylelint, prettier and add configs
    - install stylelint
    - install prettier
    - install @it-incubator/eslint-config
    - install @it-incubator/prettier-config
    - install @it-incubator/stylelint-config
    - extend eslint config with `@it-incubator/eslint-config`
    - add react rules-of-hooks to exception for stories
    - create `.prettierrc.js`, use @it-incubator/prettier-config
    - create `.stylelintrc.js`, use @it-incubator/stylelint-config
    - add tailwind imports to exception in stylelint config
    - add script to format code with prettier
    - add script to lint code with eslint and stylelint
    - format code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang={'en'}>
       <Head />
-      <body className="antialiased">
+      <body className={'antialiased'}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
