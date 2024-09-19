dbfc31dbb13a188c923531f615dcd11e64d62667
apastan <apastan.dev@gmail.com>
2024-09-19 23:44:04 +0300

    chore: remove boilerplate
    - remove folder with fonts
    - remove boilerplate from main page
    - remove boilerplate from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
-  },
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   plugins: [],
+  theme: {
+    extend: {},
+  },
 }
+
 export default config
