3e902f72b01fab14036c72a07dfd59262f542f3f
apastan <apastan.dev@gmail.com>
2024-09-27 16:01:08 +0300

    refactor: update export statement

--- src/shared/ui/recaptcha/recaptcha.tsx
+++ src/shared/ui/recaptcha/recaptcha.tsx
@@ -1,9 +1,15 @@
 import GoogleReCAPTCHA, { ReCAPTCHAProps } from 'react-google-recaptcha'
 
-export const Recaptcha = (props: Partial<ReCAPTCHAProps>) => {
+type RecaptchaProps = Partial<ReCAPTCHAProps>
+
+const Recaptcha = (props: RecaptchaProps) => {
   const key = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string
 
   const { sitekey = key, theme = 'dark', ...restProps } = props
 
   return <GoogleReCAPTCHA sitekey={sitekey} theme={theme} {...restProps} />
 }
+
+Recaptcha.displayName = 'Recaptcha'
+
+export { Recaptcha, type RecaptchaProps }
