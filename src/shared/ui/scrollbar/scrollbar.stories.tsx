c09b991fb31235b638c9053119e24ddfa0303081
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-09-29 15:32:45 +0300

    rafactor: change stories

--- src/shared/ui/scrollbar/scrollbar.stories.tsx
+++ src/shared/ui/scrollbar/scrollbar.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { ScrollArea, ScrollBar } from './scrollbar' // Путь к вашему компоненту
+import { ScrollArea } from './scrollbar' // Путь к вашему компоненту
 
-const meta: Meta<typeof ScrollBar> = {
+const meta: Meta<typeof ScrollArea> = {
   args: {},
-  component: ScrollBar,
+  component: ScrollArea,
   title: 'UI/ScrollArea',
 }
 
@@ -16,27 +16,38 @@ export const Vertical: Story = {
   args: {
     orientation: 'vertical',
   },
-  render: () => (
-    <ScrollArea className={'h-[350px] w-[200px] rounded-md border p-4'}>
-      Jokester began sneaking into the castle in the middle of the night and leaving jokes all over
-      the place: under the kings pillow, in his soup, even in the royal toilet. The king was
-      furious, but he couldnt seem to stop Jokester. And then, one day, the people of the kingdom
-      discovered that the jokes left by Jokester were so funny that they couldnt help but laugh. And
-      once they started laughing, they couldnt stop.
+  render: args => (
+    <ScrollArea
+      className={'h-[350px] w-[200px] rounded-md border p-4'}
+      orientation={args.orientation}
+    >
+      <div style={{ height: '600px' }}>
+        Jokester began sneaking into the castle in the middle of the night and leaving jokes all
+        over the place: under the kings pillow, in his soup, even in the royal toilet. The king was
+        furious, but he couldnt seem to stop Jokester. And then, one day, the people of the kingdom
+        discovered that the jokes left by Jokester were so funny that they couldnt help but laugh.
+        And once they started laughing, they couldnt stop.
+      </div>
     </ScrollArea>
   ),
 }
+
 export const Horizontal: Story = {
   args: {
     orientation: 'horizontal',
   },
-  render: () => (
-    <ScrollArea className={'h-[200px] w-[150px] rounded-md border p-4'}>
-      Jokester began sneaking into the castle in the middle of the night and leaving jokes all over
-      the place: under the kings pillow, in his soup, even in the royal toilet. The king was
-      furious, but he couldnt seem to stop Jokester. And then, one day, the people of the kingdom
-      discovered that the jokes left by Jokester were so funny that they couldnt help but laugh. And
-      once they started laughing, they couldnt stop.
+  render: args => (
+    <ScrollArea
+      className={'h-[200px] w-[150px] rounded-md border p-4'}
+      orientation={args.orientation}
+    >
+      <div style={{ whiteSpace: 'nowrap', width: '600px' }}>
+        Jokester began sneaking into the castle in the middle of the night and leaving jokes all
+        over the place: under the kings pillow, in his soup, even in the royal toilet. The king was
+        furious, but he couldnt seem to stop Jokester. And then, one day, the people of the kingdom
+        discovered that the jokes left by Jokester were so funny that they couldnt help but laugh.
+        And once they started laughing, they couldnt stop.
+      </div>
     </ScrollArea>
   ),
 }
--- src/shared/ui/scrollbar/scrollbar.tsx
+++ src/shared/ui/scrollbar/scrollbar.tsx
@@ -7,19 +7,21 @@ import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area'
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+  { orientation?: 'horizontal' | 'vertical' } & React.ComponentPropsWithoutRef<
+    typeof ScrollAreaPrimitive.Root
+  >
 >(({ children, className, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     className={cn('relative overflow-hidden', className)}
     ref={ref}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className={'h-full w-full rounded-[inherit]'}>
+    <ScrollAreaPrimitive.Viewport className={'h-full w-full'}>
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar orientation={'vertical'} />
+
     <ScrollBar orientation={'horizontal'} />
-    <ScrollAreaPrimitive.Corner />
+    <ScrollBar orientation={'vertical'} />
   </ScrollAreaPrimitive.Root>
 ))
 
@@ -31,8 +33,8 @@ const ScrollBar = React.forwardRef<
 >(({ className, orientation = 'vertical', ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     className={cn(
-      orientation === 'vertical' && 'h-full w-2.5 border-l border-l-transparent p-[1px]',
-      orientation === 'horizontal' && 'h-2.5 flex-col border-t border-t-transparent p-[1px]',
+      orientation === 'vertical' && 'flex h-full w-1 ',
+      orientation === 'horizontal' && 'flex h-1 w-full flex-col ',
       className
     )}
     orientation={orientation}
@@ -41,8 +43,8 @@ const ScrollBar = React.forwardRef<
   >
     <ScrollAreaPrimitive.ScrollAreaThumb
       className={cn(
-        orientation === 'vertical' && 'relative w-1 h-[45px] bg-light-900 rounded-sm',
-        orientation === 'horizontal' && 'relative w-[45px] h-1 bg-light-900 rounded-sm',
+        orientation === 'vertical' && 'relative flex-1 h-[45px] bg-light-900 rounded-sm',
+        orientation === 'horizontal' && 'relative flex-1 h-5 w-[45px] bg-light-900 rounded-sm',
         'transition-colors'
       )}
     />
