3aa4bd082aa3d46bfafa2c966531179ebc007df1
leonraz58 <leonraz58@gmail.com>
2024-10-07 01:14:16 +0300

    feat: add Card

---
+++ src/shared/card/card.tsx
@@ -0,0 +1,27 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import { cn } from '@/shared/utils'
+import { Slot } from '@radix-ui/react-slot'
+
+type CardProps = {
+  asChild?: boolean
+} & ComponentPropsWithoutRef<'div'>
+
+export const Card = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
+  const { asChild = false, className, ...restProps } = props
+
+  const Component = asChild ? Slot : 'div'
+
+  return (
+    <Component
+      className={cn(
+        'w-[378px]  p-6',
+        'bg-dark-500 text-light-100 border border-dark-300 rounded-sm',
+        'flex flex-col items-center',
+        className
+      )}
+      ref={ref}
+      {...restProps}
+    />
+  )
+})
