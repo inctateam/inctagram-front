0b42949251bc55c35c62c4caff6d34d92195c324
apastan <apastan.dev@gmail.com>
2024-10-08 23:14:12 +0300

    feat: add TextField component

---
+++ src/shared/ui/text-field/text-field/text-field.tsx
@@ -0,0 +1,103 @@
+import { ComponentPropsWithoutRef, ReactNode, forwardRef } from 'react'
+
+import { useGenerateId } from '@/shared/hooks'
+import { Label, Typography } from '@/shared/ui'
+import { cn } from '@/shared/utils'
+
+type TextFieldOwnProps = {
+  className?: string
+  disabled?: boolean
+  endIcon?: ReactNode
+  error?: boolean
+  helperText?: string
+  id?: string
+  label?: string
+  startIcon?: ReactNode
+}
+
+type TextFieldProps = ComponentPropsWithoutRef<'input'> & TextFieldOwnProps
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
+  const {
+    className,
+    disabled,
+    endIcon,
+    error,
+    helperText,
+    id: inputId,
+    label,
+    startIcon,
+    ...restInputProps
+  } = props
+
+  const id = useGenerateId(inputId)
+
+  return (
+    <div className={'flex flex-col w-full'}>
+      {label && (
+        <Typography
+          as={Label}
+          className={cn('text-light-900', disabled && 'text-dark-100')}
+          htmlFor={id}
+          variant={'regular14'}
+        >
+          {label}
+        </Typography>
+      )}
+      <div className={'relative'}>
+        {startIcon && (
+          <span
+            className={cn(
+              'absolute flex top-1/2 translate-y-[-50%] text-2xl disabled:text-dark-100',
+              'left-3'
+            )}
+          >
+            {startIcon}
+          </span>
+        )}
+        <input
+          className={cn(
+            'text-base font-normal text-light-100 placeholder:text-light-900 bg-transparent border border-solid border-dark-100 rounded-sm w-full px-3 py-1.5',
+            startIcon && 'pl-10',
+            endIcon && 'pr-10',
+            error && 'border-danger-500',
+            'hover:border-light-900',
+            'active:border-accent-500',
+            error && 'active:border-danger-500',
+            'focus-visible:border-accent-500 focus-visible:outline focus-visible:outline-1 focus-visible:outline-accent-500',
+            error && 'focus-visible:border-danger-500 focus-visible:outline-danger-500',
+            'disabled:text-dark-100 disabled:border-dark-100 disabled:placeholder:text-dark-100',
+            className
+          )}
+          disabled={disabled}
+          id={id}
+          type={'text'}
+          {...restInputProps}
+          ref={ref}
+        />
+        {endIcon && (
+          <span
+            className={cn(
+              'absolute flex top-1/2 translate-y-[-50%] text-2xl disabled:text-dark-100',
+              'right-3'
+            )}
+          >
+            {endIcon}
+          </span>
+        )}
+      </div>
+      {helperText && (
+        <Typography
+          className={cn('text-light-900', error && 'text-danger-500', disabled && 'text-dark-100')}
+          variant={'regular14'}
+        >
+          {helperText}
+        </Typography>
+      )}
+    </div>
+  )
+})
+
+TextField.displayName = 'TextField'
+
+export { TextField, type TextFieldProps }
