b1560605b25083dbd64066f50019cc752200fd8a
apastan <apastan.dev@gmail.com>
2024-10-10 22:30:01 +0300

    feat: add styles for focus-visible state

--- src/shared/ui/text-link/text-link.tsx
+++ src/shared/ui/text-link/text-link.tsx
@@ -13,27 +13,34 @@ type TextLinkProps = ComponentPropsWithoutRef<typeof Link> &
   TextLinkOwnProps &
   VariantProps<typeof textLinkVariants>
 
-const textLinkVariants = cva(['inline-flex items-center justify-center'], {
-  defaultVariants: {
-    color: 'primary',
-    size: 'medium',
-    underline: true,
-  },
-  variants: {
-    color: {
-      primary: 'text-accent-500',
-      regular: 'text-light-100',
+const textLinkVariants = cva(
+  [
+    'inline-flex items-center justify-center',
+    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-accent-500 focus-visible:rounded-sm focus-visible:outline-offset-2',
+    'transition',
+  ],
+  {
+    defaultVariants: {
+      color: 'primary',
+      size: 'medium',
+      underline: true,
     },
-    size: {
-      medium: 'text-sm',
-      small: 'text-xs',
+    variants: {
+      color: {
+        primary: 'text-accent-500',
+        regular: 'text-light-100',
+      },
+      size: {
+        medium: 'text-sm',
+        small: 'text-xs',
+      },
+      underline: {
+        false: 'no-underline',
+        true: 'underline',
+      },
     },
-    underline: {
-      false: 'no-underline',
-      true: 'underline',
-    },
-  },
-})
+  }
+)
 
 const TextLink = forwardRef<HTMLAnchorElement, TextLinkProps>((props, ref) => {
   const { asChild = false, className, color, size, underline, ...restProps } = props
