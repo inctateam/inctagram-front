f75398939f1fa5df1fe4390e39e158b0faf545f0
apastan <apastan.dev@gmail.com>
2024-10-11 20:49:45 +0300

    feat: add `required` indicator to TextField component

--- src/shared/ui/text-field/text-field/text-field.tsx
+++ src/shared/ui/text-field/text-field/text-field.tsx
@@ -10,8 +10,11 @@ type TextFieldOwnProps = {
   endIcon?: ReactNode
   error?: boolean
   helperText?: string
+  hideRequiredIndicator?: true
   id?: string
   label?: string
+  required?: boolean
+  requiredIndicator?: ReactNode
   startIcon?: ReactNode
 }
 
@@ -24,8 +27,11 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
     endIcon,
     error,
     helperText,
+    hideRequiredIndicator = undefined,
     id: inputId,
     label,
+    required,
+    requiredIndicator,
     startIcon,
     ...restInputProps
   } = props
@@ -59,18 +65,25 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
 
   const helperTextId = useGenerateId() + '-feedback'
 
+  const RequiredIndicator =
+    required && !hideRequiredIndicator && (requiredIndicator || defaultRequiredIndicator)
+
   return (
     <div className={'flex flex-col w-full'}>
       {label && (
-        <Typography as={Label} className={styles.label} htmlFor={id} variant={'regular14'}>
-          {label}
-        </Typography>
+        <>
+          <Typography as={Label} className={styles.label} htmlFor={id} variant={'regular14'}>
+            {label}
+            {RequiredIndicator}
+          </Typography>
+        </>
       )}
       <div className={'relative w-full'}>
         {startIcon && <span className={styles.startIcon}>{startIcon}</span>}
         <input
           aria-describedby={helperText ? helperTextId : undefined}
           aria-invalid={error ? 'true' : undefined}
+          aria-required={required ? 'true' : undefined}
           className={styles.input}
           disabled={disabled}
           id={id}
@@ -93,6 +106,12 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
     </div>
   )
 })
+
+const defaultRequiredIndicator = (
+  <span aria-hidden={'true'} className={'text-danger-500'} role={'presentation'}>
+    *
+  </span>
+)
 
 TextField.displayName = 'TextField'
 
