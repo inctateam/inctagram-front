415e15fde6cbb4f7612a8a1f8f2d73eed28d3207
leonraz58 <leonraz58@gmail.com>
2024-10-13 20:22:02 +0300

    feat: add ConfirmDialog stories

---
+++ src/shared/ui/modal/dialogs/confirm-dialog.stories.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import { Button } from '@/shared/ui'
+import { ConfirmDialog } from '@/shared/ui/modal/dialogs/confirm-dialog'
+import { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof ConfirmDialog> = {
+  component: ConfirmDialog,
+  tags: ['autodocs'],
+  title: 'UI/Modal/Dialogs',
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const WithUseState: Story = {
+  render: () => {
+    const [open, setOpen] = useState(false)
+    const onCancel = () => setOpen(false)
+    const onConfirm = () => setOpen(false)
+
+    return (
+      <>
+        <Button onClick={() => setOpen(true)}>Button With Callback</Button>
+        <ConfirmDialog
+          onCancel={onCancel}
+          onConfirm={onConfirm}
+          onOpenChange={setOpen}
+          open={open}
+          title={'Confirm'}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua
+        </ConfirmDialog>
+      </>
+    )
+  },
+}
