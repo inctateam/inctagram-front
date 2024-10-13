088c5669f0e093678d1408ac49ccf9376f750276
leonraz58 <leonraz58@gmail.com>
2024-10-13 04:26:10 +0300

    feat: add DatePicker stories

---
+++ src/shared/ui/date-picker/date-picker.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { DatePicker } from '@/shared/ui'
+
+const meta = {
+  component: DatePicker,
+  tags: ['autodocs'],
+  title: 'UI/DatePicker',
+} satisfies Meta<typeof DatePicker>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DarkMode: Story = {}
