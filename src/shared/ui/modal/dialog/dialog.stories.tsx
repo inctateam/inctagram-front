b9cf2be6d77b91fc8638dcd39e57e7a0cf81d7f3
apastan <apastan.dev@gmail.com>
2024-10-13 22:59:11 +0300

    refactor: update Dialog stories

--- src/shared/ui/modal/dialog/dialog.stories.tsx
+++ src/shared/ui/modal/dialog/dialog.stories.tsx
@@ -2,10 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { useState } from 'react'
 
-import { Button, Typography } from '@/shared/ui'
-import { Dialog } from '@/shared/ui/modal/dialog/dialog'
-import { DialogBody } from '@/shared/ui/modal/dialog/dialog-body'
-import { DialogHeader } from '@/shared/ui/modal/dialog/dialog-header'
+import { Button, Dialog, DialogBody, DialogHeader, Typography } from '@/shared/ui'
 
 const meta: Meta<typeof Dialog> = {
   component: Dialog,
@@ -49,6 +46,37 @@ export const WithTrigger: Story = {
         >
           <DialogBody className={'p-6'}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </DialogBody>
+        </Dialog>
+      </>
+    )
+  },
+}
+
+export const ManageWidth: Story = {
+  render: () => {
+    const [open, setOpen] = useState(false)
+
+    return (
+      <>
+        <Button onClick={() => setOpen(true)}>Button With Callback</Button>
+        <Dialog
+          closePosition={'inside'}
+          dialogContentProps={{ className: 'max-w-[492px]' }}
+          onOpenChange={setOpen}
+          open={open}
+        >
+          <DialogHeader>
+            <Typography as={'h2'} variant={'h1'}>
+              Modal Window
+            </Typography>
+          </DialogHeader>
+          <DialogBody className={'p-6'}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur atque autem
+            commodi cumque earum eligendi eos error explicabo hic magnam maiores, nihil numquam
+            optio perspiciatis praesentium provident quasi qui quo quod saepe soluta tenetur
+            voluptatem! Est, et magni maiores molestiae quos tempore totam veritatis? At excepturi
+            fugiat illum suscipit.
           </DialogBody>
         </Dialog>
       </>
