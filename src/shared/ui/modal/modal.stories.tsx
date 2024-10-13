39d876e22a42f84b88a952cdee8a60b9b5976da8
leonraz58 <leonraz58@gmail.com>
2024-10-13 16:58:10 +0300

    remove DialogHeader, DialogBody from base Dialog component

---
+++ src/shared/ui/modal/modal.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useState } from 'react'
+
+import { Button, Typography } from '@/shared/ui'
+import { DialogBody } from '@/shared/ui/modal/dialog-body'
+import { DialogHeader } from '@/shared/ui/modal/dialog-header'
+import { Modal } from '@/shared/ui/modal/modal'
+
+const meta: Meta<typeof Modal> = {
+  component: Modal,
+  tags: ['autodocs'],
+  title: 'UI/Modal',
+} satisfies Meta<typeof Modal>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const WithUseState: Story = {
+  render: () => {
+    const [open, setOpen] = useState(false)
+
+    return (
+      <>
+        <Button onClick={() => setOpen(true)}>Button With Callback</Button>
+        <Modal
+          className={'max-w-[378px]'}
+          closePosition={'inside'}
+          onOpenChange={setOpen}
+          open={open}
+        >
+          <DialogHeader>
+            <Typography as={'h2'} variant={'h1'}>
+              Modal Window
+            </Typography>
+          </DialogHeader>
+          <DialogBody className={'p-6'}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </DialogBody>
+        </Modal>
+      </>
+    )
+  },
+}
+
+export const WithTrigger: Story = {
+  render: () => {
+    return (
+      <>
+        <Modal
+          closePosition={'outside'}
+          trigger={<Button variant={'primary'}>Trigger Button</Button>}
+        >
+          <DialogBody className={'p-6'}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit
+          </DialogBody>
+        </Modal>
+      </>
+    )
+  },
+}
--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,11 +1,9 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 import { CloseOutline } from '@/assets/icons'
-import { IconButton, Typography } from '@/shared/ui'
-import { DialogBody } from '@/shared/ui/dialog/dialog-body'
-import { DialogClose, closeVariants } from '@/shared/ui/dialog/dialog-close'
-import { DialogContent } from '@/shared/ui/dialog/dialog-content'
-import { DialogHeader } from '@/shared/ui/dialog/dialog-header'
+import { IconButton } from '@/shared/ui'
+import { DialogClose, closeVariants } from '@/shared/ui/modal/dialog-close'
+import { DialogContent } from '@/shared/ui/modal/dialog-content'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { VariantProps } from 'class-variance-authority'
 
@@ -15,18 +13,10 @@ export type ModalProps = {
   closePosition?: VariantProps<typeof closeVariants>['closePosition']
   onOpenChange?: (open: boolean) => void
   open?: boolean
-  title?: string
   trigger?: ReactNode
 } & ComponentPropsWithoutRef<typeof DialogPrimitive.Root>
 
-export const Dialog = ({
-  children,
-  className,
-  closePosition,
-  title,
-  trigger,
-  ...props
-}: ModalProps) => {
+export const Modal = ({ children, closePosition, trigger, ...props }: ModalProps) => {
   return (
     <DialogPrimitive.Root {...props}>
       <DialogPrimitive.Trigger asChild>{trigger}</DialogPrimitive.Trigger>
@@ -36,14 +26,7 @@ export const Dialog = ({
             <CloseOutline />
           </IconButton>
         </DialogClose>
-        {title && (
-          <DialogHeader>
-            <Typography as={'h2'} variant={'h1'}>
-              {title}
-            </Typography>
-          </DialogHeader>
-        )}
-        <DialogBody className={className}>{children}</DialogBody>
+        {children}
       </DialogContent>
     </DialogPrimitive.Root>
   )
