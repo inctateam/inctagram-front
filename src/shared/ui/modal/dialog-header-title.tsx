66fd392e9277cd555cf19ee21b784a5002f70318
leonraz58 <leonraz58@gmail.com>
2024-10-13 20:21:14 +0300

    refactor: add DialogHeaderTitle

---
+++ src/shared/ui/modal/dialog-header-title.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Typography } from '@/shared/ui'
+import { DialogHeader } from '@/shared/ui/modal/dialog-header'
+
+export const DialogHeaderTitle = ({ children, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <DialogHeader {...props}>
+      <Typography as={'h2'} variant={'h1'}>
+        {children}
+      </Typography>
+    </DialogHeader>
+  )
+}
