c3dbee14906d89ce182db8c2fbef5cc5cc2893bd
apastan <apastan.dev@gmail.com>
2024-10-14 17:55:55 +0300

    refactor: update Checkbox component

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,71 +1,53 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef, useId } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
-import { CheckmarkOutline } from '@/assets/icons/components/outlined'
-import { Typography } from '@/shared/ui'
+import { CheckmarkOutline } from '@/assets/icons'
+import { useGenerateId } from '@/shared/hooks'
+import { Label, Typography } from '@/shared/ui'
 import { cn } from '@/shared/utils'
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
-import { VariantProps, cva } from 'class-variance-authority'
 
 type CheckboxProps = {
   label?: string
-} & ComponentPropsWithoutRef<typeof RadixCheckbox.Root> &
-  VariantProps<typeof checkboxVariants>
-
-const labelVariants = cva(['ml-2'], {
-  variants: {
-    disabled: {
-      false: 'text-light-500',
-      true: 'text-light-900',
-    },
-  },
-})
-
-const checkboxVariants = cva(
-  [
-    'text-light-500',
-    'w-[18px] h-[18px] m-[3px]',
-    'border-2 border-light-500 rounded',
-    'flex items-center justify-center',
-    'focus:outline-none',
-    'relative',
-    'disabled:after:content-none',
-    'disabled:border-light-900 disabled:text-light-700',
-    'after:absolute after:z-[-1] after:w-9 after:h-9 after:rounded-full',
-    'hover:after:content-[""] hover:after:bg-dark-300',
-    'focus-visible:after:content-[""] focus-visible:after:bg-dark-500',
-    'active:after:bg-dark-100',
-  ],
-  {
-    defaultVariants: {
-      checked: false,
-    },
-    variants: {
-      checked: {
-        false: ['bg-transparent', 'border-light-500', 'text-light-500'],
-        indeterminate: [''],
-        true: [
-          'bg-light-100',
-          'text-dark-900',
-          'border-light-100',
-          'disabled:border-light-900 disabled:text-light-700 disabled:bg-light-900',
-        ],
-      },
-    },
-  }
-)
+} & ComponentPropsWithoutRef<typeof RadixCheckbox.Root>
 
 const Checkbox = forwardRef<ElementRef<typeof RadixCheckbox.Root>, CheckboxProps>(
-  ({ checked, className, disabled = false, id, label, ...rest }, ref) => {
-    const randomId = useId()
-    const idForCheckbox = id || randomId
+  ({ className, disabled = false, id, label, ...rest }, ref) => {
+    const checkboxId = useGenerateId(id)
+
+    const checkboxStyles = cn(
+      [
+        ['bg-transparent', 'border-light-500', 'text-light-500'],
+        [
+          'data-[state=checked]:bg-light-100',
+          'data-[state=checked]:text-dark-100',
+          'data-[state=checked]:border-light-100',
+        ],
+        'text-light-500',
+        'w-[18px] h-[18px] m-[3px]',
+        'border-2 border-light-500 rounded',
+        'flex items-center justify-center',
+        'focus:outline-none',
+        'relative',
+        'transition',
+        'after:absolute after:z-[-1] after:w-9 after:h-9 after:rounded-full',
+        'hover:after:content-[""] hover:after:bg-dark-300',
+        'active:after:bg-dark-100',
+        'focus-visible:after:content-[""] focus-visible:after:bg-dark-500',
+        'disabled:after:content-none',
+        'disabled:border-light-900',
+        'disabled:data-[state=checked]:bg-dark-100 disabled:data-[state=checked]:border-dark-100 disabled:data-[state=checked]:text-light-700',
+      ],
+      className
+    )
+
+    const labelStyles = cn('ml-2', disabled ? 'text-light-900' : 'text-light-500')
 
     return (
       <div className={'flex'}>
         <RadixCheckbox.Root
-          checked={checked}
-          className={cn(checkboxVariants({ checked, className }))}
+          className={checkboxStyles}
           disabled={disabled}
-          id={idForCheckbox}
+          id={checkboxId}
           {...rest}
           ref={ref}
         >
@@ -74,12 +56,7 @@ const Checkbox = forwardRef<ElementRef<typeof RadixCheckbox.Root>, CheckboxProps
           </RadixCheckbox.Indicator>
         </RadixCheckbox.Root>
         {label && (
-          <Typography
-            as={'label'}
-            className={labelVariants({ disabled })}
-            htmlFor={idForCheckbox}
-            variant={'regular14'}
-          >
+          <Typography as={Label} className={labelStyles} htmlFor={checkboxId} variant={'regular14'}>
             {label}
           </Typography>
         )}
