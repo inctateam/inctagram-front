c9396d5bfef2507f8774c1d55b63069c721a95f3
leonraz58 <leonraz58@gmail.com>
2024-10-14 17:57:06 +0300

    fix: add forwardRef in date-picker

--- src/shared/ui/date-picker/date-picker-range.tsx
+++ src/shared/ui/date-picker/date-picker-range.tsx
@@ -1,27 +1,29 @@
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 import { DateRange } from 'react-day-picker'
 
 import { Calendar, TextFieldProps } from '@/shared/ui'
 import { TextFieldPopover } from '@/shared/ui/date-picker/text-field-popover'
 import { format } from 'date-fns'
 
-export const DatePickerRange = ({ ...props }: Omit<TextFieldProps, 'endIcon'>) => {
-  const [date, setDate] = useState<DateRange | undefined>(undefined)
-  const [open, setOpen] = useState<boolean>(false)
+export const DatePickerSingle = forwardRef<HTMLInputElement, Omit<TextFieldProps, 'endIcon'>>(
+  (props, ref) => {
+    const [date, setDate] = useState<DateRange | undefined>(undefined)
+    const [open, setOpen] = useState<boolean>(false)
 
-  let dateDisplay = 'Pick a date'
+    let dateDisplay = 'Pick a date'
 
-  if (date?.from) {
-    if (date.to) {
-      dateDisplay = `${format(date.from, 'dd/LL/y')} - ${format(date.to, 'dd/LL/y')}`
-    } else {
-      dateDisplay = format(date.from, 'dd/LL/y')
+    if (date?.from) {
+      if (date.to) {
+        dateDisplay = `${format(date.from, 'dd/LL/y')} - ${format(date.to, 'dd/LL/y')}`
+      } else {
+        dateDisplay = format(date.from, 'dd/LL/y')
+      }
     }
-  }
 
-  return (
-    <TextFieldPopover open={open} setOpen={setOpen} value={dateDisplay} {...props}>
-      <Calendar initialFocus mode={'range'} onSelect={setDate} selected={date} />
-    </TextFieldPopover>
-  )
-}
+    return (
+      <TextFieldPopover open={open} setOpen={setOpen} value={dateDisplay} {...props} ref={ref}>
+        <Calendar initialFocus mode={'range'} onSelect={setDate} selected={date} />
+      </TextFieldPopover>
+    )
+  }
+)
--- src/shared/ui/date-picker/date-picker-single.tsx
+++ src/shared/ui/date-picker/date-picker-single.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 
 import { Calendar, TextFieldProps } from '@/shared/ui'
 import { TextFieldPopover } from '@/shared/ui/date-picker/text-field-popover'
 import { format } from 'date-fns'
 
-export const DatePickerSingle = ({ ...props }: Omit<TextFieldProps, 'endIcon'>) => {
-  const [date, setDate] = useState<Date | undefined>(undefined)
-  const [open, setOpen] = useState<boolean>(false)
+export const DatePickerSingle = forwardRef<HTMLInputElement, Omit<TextFieldProps, 'endIcon'>>(
+  (props, ref) => {
+    const [date, setDate] = useState<Date | undefined>(undefined)
+    const [open, setOpen] = useState<boolean>(false)
 
-  let dateDisplay = 'Pick a date'
+    let dateDisplay = 'Pick a date'
 
-  if (date) {
-    dateDisplay = format(date, 'dd/LL/y')
+    if (date) {
+      dateDisplay = format(date, 'dd/LL/y')
+    }
+
+    return (
+      <TextFieldPopover open={open} setOpen={setOpen} value={dateDisplay} {...props} ref={ref}>
+        <Calendar initialFocus mode={'single'} onSelect={setDate} selected={date} />
+      </TextFieldPopover>
+    )
   }
-
-  return (
-    <TextFieldPopover open={open} setOpen={setOpen} value={dateDisplay} {...props}>
-      <Calendar initialFocus mode={'single'} onSelect={setDate} selected={date} />
-    </TextFieldPopover>
-  )
-}
+)
