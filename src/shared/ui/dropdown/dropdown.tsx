d3aa7404cba3b6f7b04d6f2d3846e4fea65914bf
apelsinom <apelsinom@mail.ru>
2024-10-16 22:55:34 +0300

    IN-22 feat: add component Dropdown - stories

--- src/shared/ui/dropdown/dropdown.tsx
+++ src/shared/ui/dropdown/dropdown.tsx
@@ -1,66 +1,92 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
+'use client'
 
-import { LogOutOutline, Person } from '@/assets/icons'
-import { Typography } from '@/shared/ui'
+import * as React from 'react'
+import { ComponentPropsWithoutRef, ReactNode, useState } from 'react'
+
+import { MoreHorizontalOutline } from '@/assets/icons'
+import { Button } from '@/shared/ui'
 import { cn } from '@/shared/utils'
-import * as DropdownMenuRadix from '@radix-ui/react-dropdown-menu'
-import Image from 'next/image'
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 
-type Props = {
-  email: string
-  name: string
-  photo: string
-  photoDesc: string
-  profilePageHref: string
+type DropdownMenuItemProps = {
+  icon: ReactNode
+  label: string
+  onClick?: () => void
 }
 
-export const Dropdown = ({ email, name, photo, photoDesc, profilePageHref }: Props) => {
+type Props = {
+  items: DropdownMenuItemProps[]
+} & ComponentPropsWithoutRef<typeof DropdownMenu>
+
+export const Dropdown = ({ items }: Props) => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <DropdownMenu trigger={<Image alt={photoDesc} src={photo} />}>
-      <DropdownItem>
-        <div>
-          <Image alt={photoDesc} src={photo} />
-          <div>
-            <Typography>{name}</Typography>
-            <Typography>{email}</Typography>
-          </div>
-        </div>
-      </DropdownItem>
-      <DropdownSeparator />
-      <DropdownItem asChild>
-        <Person />
-        <a href={profilePageHref}>My profile</a>
-      </DropdownItem>
-      <DropdownSeparator />
-      <DropdownItem>
-        <LogOutOutline />
-        Sing Out
-      </DropdownItem>
+    <DropdownMenu onOpenChange={setOpen}>
+      <DropdownMenuTrigger className={'focus:outline-none'}>
+        <MoreHorizontalOutline
+          className={cn(
+            'w-6 h-6 hover:text-blue-500 active:text-blue-500',
+            open && 'text-blue-500'
+          )}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align={'end'}>
+        {items.map((item, i) => (
+          <DropdownMenuItem key={i}>
+            <Button
+              className={'text-light-100'}
+              onClick={item.onClick}
+              startIcon={item.icon}
+              variant={'text'}
+            >
+              {item.label}
+            </Button>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
 
-type MenuProps = {
-  children?: ReactNode
-  trigger?: ReactNode
-} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Root>
-const DropdownMenu = ({ children, trigger, ...rest }: MenuProps) => {
-  return (
-    <DropdownMenuRadix.Root {...rest}>
-      <DropdownMenuRadix.Trigger>{trigger}</DropdownMenuRadix.Trigger>
-      <DropdownMenuRadix.Portal>
-        <DropdownMenuRadix.Content className={''} sideOffset={5}>
-          {children}
-        </DropdownMenuRadix.Content>
-      </DropdownMenuRadix.Portal>
-    </DropdownMenuRadix.Root>
-  )
-}
+const DropdownMenu = DropdownMenuPrimitive.Root
 
-type ItemProps = ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
-const DropdownItem = ({ className, ...rest }: ItemProps) => {
-  return <DropdownMenuRadix.Item className={cn('m-0', className)} {...rest} />
-}
-const DropdownSeparator = () => {
-  return <DropdownMenuRadix.Separator className={cn('m-0')} />
-}
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+
+const DropdownMenuContent = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <DropdownMenuPrimitive.Portal>
+    <DropdownMenuPrimitive.Content
+      className={cn(
+        'z-50 min-w-[8rem] overflow-hidden rounded-md border border-dark-100 bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        className
+      )}
+      ref={ref}
+      sideOffset={sideOffset}
+      {...props}
+    />
+  </DropdownMenuPrimitive.Portal>
+))
+
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
+
+const DropdownMenuItem = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Item>,
+  {
+    inset?: boolean
+  } & React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item>
+>(({ className, inset, ...props }, ref) => (
+  <DropdownMenuPrimitive.Item
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      inset && 'pl-8',
+      className
+    )}
+    ref={ref}
+    {...props}
+  />
+))
+
+DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
