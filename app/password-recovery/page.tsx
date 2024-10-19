b133f5006369d8ffc750619c8ae145d3fb22f1c0
apastan <apastan.dev@gmail.com>
2024-10-19 21:10:13 +0300

    feat: add routes for auth

---
+++ app/password-recovery/page.tsx
@@ -0,0 +1,5 @@
+const PasswordReset = () => {
+  return 'PasswordReset'
+}
+
+export default PasswordReset
---
+++ app/password-reset/page.tsx
@@ -0,0 +1,5 @@
+const PasswordRecovery = () => {
+  return 'PasswordRecovery'
+}
+
+export default PasswordRecovery
