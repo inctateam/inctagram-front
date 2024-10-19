0b9b46de25f76b8bb6c5d5a9209db5a15db3ab41
leonraz58 <leonraz58@gmail.com>
2024-10-20 01:40:07 +0300

    feat: add ProgressBar component

---
+++ src/shared/ui/progress-bar/progress-bar.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/shared/utils'
+
+export const ProgressBar = () => {
+  return (
+    <div className={'absolute top-0 overflow-hidden w-full h-[4px] bg-accent-100'}>
+      <div
+        className={cn(
+          'relative w-full h-full',
+          'before:content-[""] before:absolute before:h-full before:bg-accent-700 before:animate-[progressBar1_1.5s_infinite_ease-in-out]',
+          'before:content-[""] after:absolute after:h-full after:bg-accent-700 after:animate-[progressBar2_1.5s_infinite_ease-in]'
+        )}
+      ></div>
+    </div>
+  )
+}
