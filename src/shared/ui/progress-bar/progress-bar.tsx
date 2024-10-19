5e7eff65fd790e84e6c1b8ee9b1d93e565292bce
leonraz58 <leonraz58@gmail.com>
2024-10-20 02:16:27 +0300

    refactor: ProgressBar styles

--- src/shared/ui/progress-bar/progress-bar.tsx
+++ src/shared/ui/progress-bar/progress-bar.tsx
@@ -1,15 +1,16 @@
-import { cn } from '@/shared/utils'
-
 export const ProgressBar = () => {
   return (
     <div className={'absolute top-0 overflow-hidden w-full h-[4px] bg-accent-100'}>
-      <div
-        className={cn(
-          'relative w-full h-full',
-          'before:content-[""] before:absolute before:h-full before:bg-accent-700 before:animate-[progressBar1_1.5s_infinite_ease-in-out]',
-          'after:content-[""] after:absolute after:h-full after:bg-accent-700 after:animate-[progressBar2_1.5s_infinite_ease-in]'
-        )}
-      ></div>
+      <div className={'relative w-full h-full'}>
+        <span
+          className={
+            'absolute h-full bg-accent-700 animate-[progressBar1_1.5s_infinite_ease-in-out]'
+          }
+        ></span>
+        <span
+          className={'absolute h-full bg-accent-700 animate-[progressBar2_1.5s_infinite_ease-in]'}
+        ></span>
+      </div>
     </div>
   )
 }
