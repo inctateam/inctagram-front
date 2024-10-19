8b3a0e7aeaa5392d67ecdde2ae037d3b536d02e5
apastan <apastan.dev@gmail.com>
2024-10-19 14:36:33 +0300

    chore: migrate to AppRouter
    - remove pages folder
    - add app folder
    - update tsconfig config
    - chore: update tailwind config
    - chore: add 'use client' for UI-kit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+import '@/styles/globals.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang={'en'}>
+      <body>{children}</body>
+    </html>
+  )
+}
