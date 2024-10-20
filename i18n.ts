26bc40fd735d45c50b9b8d4f4b4d4bc9e0f7749e
apastan <apastan.dev@gmail.com>
2024-10-20 22:20:48 +0300

    feat: add i18n config

---
+++ i18n.ts
@@ -0,0 +1,17 @@
+import { notFound } from 'next/navigation'
+import { getRequestConfig } from 'next-intl/server'
+
+import { type Locale, locales } from './i18n.config'
+
+// Load the translation file for the active locale
+// on each request and make it available to our
+// pages, components, etc.
+export default getRequestConfig(async ({ locale }) => {
+  if (!locales.includes(locale as Locale)) {
+    return notFound()
+  }
+
+  return {
+    messages: (await import(`./src/locales/${locale}.json`)).default,
+  }
+})
