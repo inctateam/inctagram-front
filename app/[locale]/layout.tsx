2e686a976dabc3367dbd5425ba75246c3f2f233b
apastan <apastan.dev@gmail.com>
2024-10-20 22:33:13 +0300

    feat: update RootLayout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 import { ToastProvider } from '@/shared/ui'
+import { NextIntlClientProvider } from 'next-intl'
 
 import '@/styles/globals.css'
 import '@fontsource/inter/400.css'
@@ -8,12 +9,20 @@ import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: ReactNode
+  params: { locale: string }
+}) {
   return (
-    <html lang={'en'}>
+    <html lang={locale}>
       <body>
-        <ToastProvider />
-        {children}
+        <NextIntlClientProvider locale={locale}>
+          <ToastProvider />
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   )
