ed89ec383a333724d9f715c2ed02aa5d31144b67
apastan <apastan.dev@gmail.com>
2024-10-21 10:25:59 +0300

    feat: example of text translation

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -3,8 +3,11 @@ import { PATH } from '@/shared/constants'
 import { TextLink } from '@/shared/ui'
 import { cn } from '@/shared/utils'
 import { LocaleSwitcher } from '@/widgets'
+import { useTranslations } from 'next-intl'
 
 export const Header = () => {
+  const t = useTranslations('Header')
+
   return (
     <div className={cn('sticky top-0', 'w-full h-[59px]', 'border-b border-dark-300 border-solid')}>
       <LayoutContainer className={'h-full flex items-center justify-between'}>
@@ -19,20 +22,20 @@ export const Header = () => {
 
         {/* Remove later*/}
         <div className={'flex gap-5'}>
-          <TextLink href={PATH.SIGN_UP}>Sigh up</TextLink>
+          <TextLink href={PATH.SIGN_UP}>{t('signUp')}</TextLink>
           <TextLink href={PATH.CONFIRM_EMAIL + '?token=983274981-27390128039-8213098-3459083495'}>
-            Confirm Email
+            {t('confirmEmail')}
           </TextLink>
 
-          <TextLink href={PATH.SIGN_IN}>Sigh in</TextLink>
+          <TextLink href={PATH.SIGN_IN}>{t('signIn')}</TextLink>
 
-          <TextLink href={PATH.PASSWORD_RECOVERY}>Forgot password</TextLink>
+          <TextLink href={PATH.PASSWORD_RECOVERY}>{t('passwordRecovery')}</TextLink>
           <TextLink href={PATH.PASSWORD_RESET + '?token=2348230-842084-23093482039-23492390'}>
-            Reset password
+            {t('resetPassword')}
           </TextLink>
 
-          <TextLink href={PATH.TERMS_OF_SERVICE}>Terms of service</TextLink>
-          <TextLink href={PATH.PRIVACY_POLICY}>Privacy Policy</TextLink>
+          <TextLink href={PATH.TERMS_OF_SERVICE}>{t('terms')}</TextLink>
+          <TextLink href={PATH.PRIVACY_POLICY}>{t('privacyPolicy')}</TextLink>
         </div>
         {/* Remove later*/}
 
