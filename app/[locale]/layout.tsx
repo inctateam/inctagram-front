7483e3122365fc2c7a728d03db0ff2f9f3519657
apastan <apastan.dev@gmail.com>
2024-11-03 10:52:11 +0300

    fix [i18n]: messages provider for client components

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,8 @@
-'use client'
 import { ReactNode } from 'react'
 
-import { StoreProvider } from '@/services'
-import { ToastProvider } from '@/shared/ui'
-import { NextIntlClientProvider } from 'next-intl'
+import { AppLocale, LOCALES } from '@/i18n/i18n.config'
+import { notFound } from 'next/navigation'
+import { getMessages } from 'next-intl/server'
 
 import '@/styles/globals.css'
 import '@fontsource/inter/400.css'
@@ -11,22 +10,27 @@ import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
 
-export default function RootLayout({
+import { ClientRootLayout } from './client-root-layout'
+
+export default async function ServerSideLayout({
   children,
   params: { locale },
 }: {
   children: ReactNode
   params: { locale: string }
 }) {
+  if (!LOCALES.includes(locale as AppLocale)) {
+    notFound()
+  }
+
+  const messages = await getMessages()
+
   return (
     <html lang={locale}>
       <body>
-        <StoreProvider>
-          <NextIntlClientProvider locale={locale}>
-            <ToastProvider />
-            {children}
-          </NextIntlClientProvider>
-        </StoreProvider>
+        <ClientRootLayout locale={locale} messages={messages}>
+          {children}
+        </ClientRootLayout>
       </body>
     </html>
   )
