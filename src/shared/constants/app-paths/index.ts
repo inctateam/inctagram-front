8c2f02592bbc197fed0ccf9263ced0fc4ea3765d
leonraz58 <leonraz58@gmail.com>
2025-01-30 04:20:08 +0300

    chore: change deploy url

--- src/shared/constants/app-paths/index.ts
+++ src/shared/constants/app-paths/index.ts
@@ -12,4 +12,4 @@ export const PATH = {
 export const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://inctagram-front-gamma.vercel.app/'
+    : 'https://content-universe.store/'
