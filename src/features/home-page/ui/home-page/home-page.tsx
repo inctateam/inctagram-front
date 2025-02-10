21545fabc9fac4082fb65e9087cd17ddd9821f62
Olesya Shalay <olesyakublickaya@yandex.ru>
2025-02-10 12:40:32 +0300

    ref: fix folders structure

--- src/features/home-page/ui/home-page/home-page.tsx
+++ src/features/home-page/ui/home-page/home-page.tsx
@@ -4,7 +4,7 @@ import { useMeQuery } from '@/features/auth/api'
 import { ProgressBar } from '@/shared/ui'
 
 import { PublicPage } from '../public-page'
-import { ProfilePage } from './user-profile/profile-page'
+import { ProfilePage } from '../user-profile/profile-page'
 
 export const HomePage = () => {
   const { data, error, isLoading } = useMeQuery()
