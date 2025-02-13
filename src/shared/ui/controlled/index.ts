49d29c85f99c021439594204ebf28b81601f5550
leonraz58 <leonraz58@gmail.com>
2025-02-14 01:43:10 +0300

    feat: add ControlledTextarea

--- src/shared/ui/controlled/index.ts
+++ src/shared/ui/controlled/index.ts
@@ -1,3 +1,4 @@
 export * from './controlled-checkbox/controlled-checkbox'
 export * from './controlled-passwordtextfield/controlled-passwordtextfield'
+export * from './controlled-textarea/controlled-textarea'
 export * from './controlled-textfield/controlled-textfield'
