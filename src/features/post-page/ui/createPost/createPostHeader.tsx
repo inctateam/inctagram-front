bc8a0b56155481da4ed0a1c552683188021db06f
leonraz58 <leonraz58@gmail.com>
2025-02-14 01:49:49 +0300

    feat: add publish variant to CreatePostHeader

--- src/features/post-page/ui/createPost/createPostHeader.tsx
+++ src/features/post-page/ui/createPost/createPostHeader.tsx
@@ -4,19 +4,38 @@ import { Button, DialogHeader, IconButton, Typography } from '@/shared/ui'
 type CreatePostHeaderProps = {
   handleBack: () => void
   handleNext: () => void
+  publish?: boolean
   title: string
 }
 
-export const CreatePostHeader = ({ handleBack, handleNext, title }: CreatePostHeaderProps) => {
+export const CreatePostHeader = ({
+  handleBack,
+  handleNext,
+  publish,
+  title,
+}: CreatePostHeaderProps) => {
   return (
     <DialogHeader className={'flex justify-center items-center relative'}>
       <IconButton className={'hover:bg-dark-100/70 absolute left-6'} onClick={() => handleBack()}>
         <ArrowBackOutline />
       </IconButton>
       <Typography variant={'h1'}>{title}</Typography>
-      <Button className={'absolute right-6'} onClick={() => handleNext()} variant={'text'}>
-        Next
-      </Button>
+      {!publish && (
+        <Button className={'absolute right-6'} onClick={() => handleNext()} variant={'text'}>
+          Next
+        </Button>
+      )}
+      {publish && (
+        <Button
+          className={'absolute right-6'}
+          form={'publish-form'}
+          onClick={() => handleNext()}
+          type={'submit'}
+          variant={'text'}
+        >
+          Publish
+        </Button>
+      )}
     </DialogHeader>
   )
 }
