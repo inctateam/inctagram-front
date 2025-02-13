38abe9ed9b7631b193a82456b0f638aa952f32cb
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2025-02-13 13:57:17 +0300

    POST-MODAL feat: test modal page

--- app/[locale]/(main)/@modal/(.)posts/[id]/page.tsx
+++ app/[locale]/(main)/@modal/(.)posts/[id]/page.tsx
@@ -1,33 +1,31 @@
 'use client'
 import { usePostQuery } from '@/features/post-page/api'
 import { PostModal } from '@/features/post-page/ui/post'
-import { ImageContent } from '@/shared/ui/image-content'
+import { ProgressBar } from '@/shared/ui'
 // import { Dialog, DialogBody, DialogHeader, Typography } from '@/shared/ui'
+import { ImageContent } from '@/shared/ui/image-content'
 import { useRouter } from 'next/navigation'
 
 export default function Post({ params: { id } }: { params: { id: string } }) {
   const router = useRouter()
-  const { data: post } = usePostQuery({ postId: Number(id) })
+  const { data: post, isLoading } = usePostQuery({ postId: Number(id) })
 
-  // if (isLoading) {
-  //   return <ProgressBar />
-  // }
-  // if (isError) {
-  //   toast.error('error')
-  // }
+  console.log('🔵 ModalPage рендерится для поста', id)
+  if (isLoading) {
+    return <ProgressBar />
+  }
 
   return (
     <PostModal onOpenChange={() => router.back()} open post={post!}>
       <ImageContent itemImages={post?.images || []} />
     </PostModal>
-
-    // <Dialog closePosition={'inside'} onOpenChange={() => router.back()} open>
-    //   <DialogHeader>
-    //     <Typography variant={'h2'}>Post {id}</Typography>
-    //   </DialogHeader>
-    //   <DialogBody className={'p-6'}>
-    //     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    //   </DialogBody>
-    // </Dialog>
   )
 }
+// <Dialog closePosition={'inside'} onOpenChange={() => router.back()} open>
+//   <DialogHeader>
+//     <Typography variant={'h2'}>Post {id}</Typography>
+//   </DialogHeader>
+//   <DialogBody className={'p-6'}>
+//     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+//   </DialogBody>
+// </Dialog>
--- app/[locale]/(main)/posts/[id]/page.tsx
+++ app/[locale]/(main)/posts/[id]/page.tsx
@@ -2,29 +2,23 @@ export default function Posts({ params: { id } }: { params: { id: string } }) {
   return <div>Post {id}</div>
 }
 // 'use client'
-// import { useState } from 'react'
-//
 // import { usePostQuery } from '@/features/post-page/api'
 // import { PostModal } from '@/features/post-page/ui/post'
+// import { ProgressBar } from '@/shared/ui'
 // import { ImageContent } from '@/shared/ui/image-content'
+// import { useRouter } from 'next/navigation'
 //
 // export default function Posts({ params: { id } }: { params: { id: string } }) {
-//   const [open, setOpen] = useState(true)
-//   const { data: post } = usePostQuery({ postId: Number(id) })
+//   const router = useRouter()
+//   const { data: post, isLoading } = usePostQuery({ postId: +id })
+//
+//   if (isLoading) {
+//     return <ProgressBar />
+//   }
 //
 //   return (
-//     <div>
-//       <div
-//         className={'flex cursor-pointer border-2 border-dark-100'}
-//         onClick={() => setOpen(!open)}
-//       >
-//         Post {id}
-//       </div>
-//       {open && post && (
-//         <PostModal onOpenChange={() => setOpen(!open)} open={open} post={post}>
-//           <ImageContent itemImages={post.images} />
-//         </PostModal>
-//       )}
-//     </div>
+//     <PostModal onOpenChange={() => router.back()} open post={post!}>
+//       <ImageContent itemImages={post?.images || []} />
+//     </PostModal>
 //   )
 // }
