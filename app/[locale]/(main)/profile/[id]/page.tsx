3ad485231c8932459157be07ab0b95b819b46bee
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2025-02-14 09:29:43 +0300

    POST-MODAL feat: test modal page

--- app/[locale]/(main)/profile/[id]/page.tsx
+++ app/[locale]/(main)/profile/[id]/page.tsx
@@ -1,21 +1,31 @@
-import { Card } from '@/shared/ui'
-import Link from 'next/link'
+'use client'
+import { useMeQuery } from '@/features/auth/api'
+import { ProfilePage } from '@/features/home-page/ui/user-profile/profile-page'
+import { ProgressBar } from '@/shared/ui'
+// import Link from 'next/link'
 
 export default function Profile({ params: { id } }: { params: { id: string } }) {
+  const { data: me, isLoading } = useMeQuery()
+
+  if (isLoading) {
+    return <ProgressBar />
+  }
+
   return (
     <div>
-      Profile {id}
-      <div className={'w-full flex gap-1 flex-wrap'}>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <div className={'w-full max-w-xs aspect-square'} key={index}>
-            <Link href={`/posts/${index + 1}`}>
-              <Card className={'flex items-center justify-center w-full h-full'}>
-                Post {index + 1}
-              </Card>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <ProfilePage isAuth={!!me?.userId} userId={me?.userId} />
+      {/*Profile {id}*/}
+      {/*<div className={'w-full flex gap-1 flex-wrap'}>*/}
+      {/*  {Array.from({ length: 5 }).map((_, index) => (*/}
+      {/*    <div className={'w-full max-w-xs aspect-square'} key={index}>*/}
+      {/*      <Link href={`/posts/${4900}`}>*/}
+      {/*        <Card className={'flex items-center justify-center w-full h-full'}>*/}
+      {/*          Post {index + 1}*/}
+      {/*        </Card>*/}
+      {/*      </Link>*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
     </div>
   )
 }
