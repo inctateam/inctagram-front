9598c9fdef272f8fdce53cd5b2d19bcdc3bf7815
leonraz58 <leonraz58@gmail.com>
2025-02-17 02:41:01 +0300

    fix: remove profile redirect from HomePage

--- src/features/home-page/ui/home-page/home-page.tsx
+++ src/features/home-page/ui/home-page/home-page.tsx
@@ -1,20 +1,5 @@
-'use client'
-
-import { useMeQuery } from '@/features/auth/api'
-import { ProgressBar } from '@/shared/ui'
-
 import { PublicPage } from '../public-page'
-import { ProfilePage } from '../user-profile/profile-page'
 
 export const HomePage = () => {
-  const { data, error, isLoading } = useMeQuery()
-
-  if (error || !data) {
-    return <PublicPage />
-  }
-  if (isLoading) {
-    return <ProgressBar />
-  }
-
-  return <ProfilePage userId={data.userId} />
+  return <PublicPage />
 }
