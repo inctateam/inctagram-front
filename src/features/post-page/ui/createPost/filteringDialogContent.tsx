dacf523a664a5ba1a5a949b18cbb0ad5c29cefb4
leonraz58 <leonraz58@gmail.com>
2025-03-05 03:53:52 +0300

    feat: add FilteringDialogContent

---
+++ src/features/post-page/ui/createPost/filteringDialogContent.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+import { CreatePostHeader } from '@/features/post-page/ui/createPost/createPostHeader'
+import {
+  createPostSliceActions,
+  createPostSliceSelectors,
+} from '@/features/post-page/ui/createPost/createPostSlice'
+import { FILTERS } from '@/features/post-page/ui/createPost/filters'
+import { getFilteredImage } from '@/features/post-page/ui/createPost/getFilteredImage'
+import { useAppDispatch, useAppSelector } from '@/services'
+import { DialogBody } from '@/shared/ui'
+import { ImageContent } from '@/shared/ui/image-content'
+import Image from 'next/image'
+
+type FilteringDialogContentProps = {
+  handleBack: () => void
+  handleNext: () => void
+}
+
+export const FilteringDialogContent = ({ handleBack, handleNext }: FilteringDialogContentProps) => {
+  const dispatch = useAppDispatch()
+  const images = useAppSelector(createPostSliceSelectors.selectImages)
+
+  const [currentImage, setCurrentImage] = useState<number>(0)
+
+  const setFilterHandler = async (filter: string) => {
+    const link = await getFilteredImage(images[currentImage], filter)
+
+    dispatch(createPostSliceActions.setImage({ image: link, index: currentImage }))
+  }
+
+  return (
+    <div className={'w-[972px] h-[564px] flex flex-col'}>
+      <CreatePostHeader handleBack={handleBack} handleNext={handleNext} title={'Filtering'} />
+      <DialogBody className={'flex flex-grow'}>
+        <div className={'w-1/2 h-full flex'}>
+          <ImageContent itemImages={images} selectedIndexCallBack={setCurrentImage}></ImageContent>
+        </div>
+        <div className={'w-1/2 h-full flex flex-wrap gap-6 justify-center py-6'}>
+          {FILTERS.map((filter, index) => (
+            <div className={''} key={index}>
+              <Image
+                alt={'123'}
+                className={'object-cover block aspect-square'}
+                height={108}
+                onClick={() => setFilterHandler(filter.value)}
+                src={images[currentImage]}
+                style={{ filter: filter.value }}
+                width={108}
+              />
+            </div>
+          ))}
+        </div>
+      </DialogBody>
+    </div>
+  )
+}
