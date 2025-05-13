370164beffb52a842de4f6da0c6c388154ac8358
apelsinom <apelsinom@mail.ru>
2025-05-13 22:28:03 +0300

    fix: authorization Google end GitHub

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -1,32 +1,12 @@
 'use client'
 
-// import { useEffect } from 'react'
-
 import { useMeQuery } from '@/features/auth/api'
 import { HomePage } from '@/features/home-page/ui/home-page'
 import { PublicPage } from '@/features/home-page/ui/public-page'
 import { Spinner } from '@/shared/ui'
-import { useRouter, useSearchParams } from 'next/navigation'
 
 export default function Home() {
-  const searchParams = useSearchParams()
-  const router = useRouter()
   const { data: me, error, isLoading } = useMeQuery()
-
-  // useEffect(() => {
-  //   if (me) {
-  //     router.push(`/profile/${me.userId}`)
-  //   }
-  // }, [me, router])
-
-  // 🔹 АВТОРИЗАЦИЯ GOOGLE ПРОДОЛЖАЕТСЯ И ЗАКАНЧИВАЕТСЯ НА СТРАНИЦЕ baseUrl/auth/google
-  const code = searchParams.get('code')
-
-  if (code) {
-    router.push(`/auth/google?code=${code}`)
-
-    return null
-  }
 
   if (isLoading) {
     return <Spinner fullScreen />
